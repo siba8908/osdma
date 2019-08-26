53c689e5d7c478f0d8ecf8c33ad9d21441a21335
smruti <smrutiranjan.rout936@gmail.com>
2019-08-26 23:20:14 +0530

    commit by smruti ranjan

---
+++ src/main/java/com/sunjray/osdma/PMrepository/SiteLocationRepository.java
@@ -0,0 +1,9 @@
+package com.sunjray.osdma.PMrepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sunjray.osdma.PMmodel.SiteLocation;
+
+public interface SiteLocationRepository extends JpaRepository<SiteLocation, Long> {
+
+}
